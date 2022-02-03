35389e4bc215445c34f2fb9215986e85f256c2cc
Ritu <Ritu2bandi@gmail.com>
2022-02-04 01:32:34 +0530

    basic problems of for loops

---
+++ multiple.js
@@ -0,0 +1,22 @@
+var i;
+for(i=0; i<=15; i++)
+
+{
+    if(i%3==0 && i%5 ==0)
+    {
+        console.log("FizzBuzz");
+    }
+    else
+    if(i%3==0)
+    {
+        console.log("Fizz");
+    }
+    else if(i%5==0)
+    {
+        console.log("Buzz");
+    }
+    else if (i%3 !==0 && i%5 !==0)
+    {
+        console.log("Number");
+    }
+}
